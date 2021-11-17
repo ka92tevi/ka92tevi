c417e47cee8a97698a20bebc2b39ce6abf5f519d
ka92tevi <rituparna.bose@fau.de>
2021-11-17 15:16:27 +0100

    comments: committing my first package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(name='ka92tevi',
+version='0.1',
+author='DSSS',
+author_email='rituparna.bose@fau.de',
+packages=find_packages(),
+install_requires=['numpy', 'Pillow', 'ipywidgets'])
